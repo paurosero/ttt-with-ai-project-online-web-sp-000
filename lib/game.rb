79e1e73837777a06bfb85c473548ab303211ed77
Paul Rosero <paul_rosero@yahoo.com>
2019-06-17 00:16:09 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -11,36 +11,48 @@ class Game
   end
   
   def current_player
-    board.turn_count
-  
+    board.turn_count % 2 == 0 ? player_1 : player_2
   end 
   
   def won?
-    
+    WIN_COMBINATIONS.detect do |win_combo|
+      @board.cells[win_combo[0]] == @board.cells[win_combo[1]] && @board.cells[win_combo[2]] == @board.cells[win_combo[1]] && @board.cells[win_combo[0]] != " "
+    end
   end 
   
   def draw?
-    
+    !won? && board.full?
   end
   
   def over?
-    
+    draw? || won? || board.full?
   end
   
   def winner
-    
+    if won?
+      board.cells[won?[0]] 
+    end
   end
   
-  def turn 
-    
-  end 
+  def turn
+    input = current_player.move(board)
+    if board.valid_move?(input)
+      board.update(input, current_player)
+    else
+      turn
+    end
+  end
   
   def play 
-    
+    while !over? 
+     turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+     puts "Cat's Game!"
+    end
   end
   
 end
-
-
-
 
